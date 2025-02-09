a348fe5a94328e83213b226fcc5a0eec6b4a9675
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-02-09 06:19:42 -0400

    Add Obsidian.nvim

---
+++ .config/nvim/lua/allisnull/plugins/obsidian.lua
@@ -0,0 +1,35 @@
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    event = {
+        "BufReadPre /home/allisnull/Vault/*.md",
+        "BufNewFile /home/allisnull/Vault/*.md",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        -- "hrsh7th/nvim-cmp",
+        -- "nvim-telescope/telescope.nvim",
+        -- "nvim-treesitter/nvim-treesitter",
+    },
+    mappings = {
+        ["gf"] = {
+            action = function()
+                return require("obsidian").util.gf_passthrough()
+            end,
+            opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>cb"] = {
+            action = function()
+                return require("obsidian").util.toggle_checkbox()
+            end,
+            opts = { buffer = true },
+        },
+        ["<CR>"] = {
+            action = function()
+                return require("obsidian").util.smart_action()
+            end,
+            opts = { buffer = true, expr = true },
+        }
+    }
+}
