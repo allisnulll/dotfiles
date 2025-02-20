9e59b1594d38097193e4976ba431d85130aa3041
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-02-20 19:19:47 -0400

    Finish obsidian.nvim config

--- .config/nvim/lua/allisnull/plugins/ltex-utils.lua
+++ .config/nvim/lua/allisnull/plugins/ltex-utils.lua
@@ -5,7 +5,7 @@ return {
         "nvim-telescope/telescope.nvim",
         "nvim-telescope/telescope-fzf-native.nvim",
     },
-    -- From PR https://github.com/jhofscheier/ltex-utils.nvim/pull/21/commits
+    -- NOTE: From PR https://github.com/jhofscheier/ltex-utils.nvim/pull/21/commits
     commit = "f9bc136ca20f38233826faee7854d944fcaf49ac",
     opts = {},
 }
--- .config/nvim/lua/allisnull/plugins/obsidian.lua
+++ .config/nvim/lua/allisnull/plugins/obsidian.lua
@@ -3,8 +3,8 @@ return {
     version = "*",
     lazy = true,
     event = {
-        "BufReadPre /home/allisnull/Vault/*.md",
-        "BufNewFile /home/allisnull/Vault/*.md",
+        "BufReadPre " .. vim.fn.expand("~") .. "/Vault/*.md",
+        "BufNewFile " .. vim.fn.expand("~") .. "/Vault/*.md",
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
@@ -12,24 +12,54 @@ return {
         "nvim-telescope/telescope.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
-    mappings = {
-        ["gf"] = {
-            action = function()
-                return require("obsidian").util.gf_passthrough()
-            end,
-            opts = { noremap = false, expr = true, buffer = true },
-        },
-        ["<leader>cb"] = {
-            action = function()
-                return require("obsidian").util.toggle_checkbox()
-            end,
-            opts = { buffer = true },
-        },
-        ["<CR>"] = {
-            action = function()
-                return require("obsidian").util.smart_action()
-            end,
-            opts = { buffer = true, expr = true },
-        }
-    }
+    attachments = { img_folder = "assets" },
+    config = function()
+        local obsidian = require("obsidian")
+
+        obsidian.setup({
+            workspaces = {{
+                name = "Vault",
+                path = "~/Vault",
+            }},
+            mappings = {
+                ["gf"] = {
+                    action = function()
+                        return require("obsidian").util.gf_passthrough()
+                    end,
+                    opts = { noremap = false, expr = true, buffer = true },
+                },
+                ["<leader>cb"] = {
+                    action = function()
+                        return require("obsidian").util.toggle_checkbox()
+                    end,
+                    opts = { buffer = true, desc = "Toggle checkbox" },
+                },
+                ["<CR>"] = {
+                    action = function()
+                        return require("obsidian").util.smart_action()
+                    end,
+                    opts = { buffer = true, expr = true },
+                },
+            },
+            daily_notes = { folder = "dailies" },
+        })
+
+        vim.keymap.set("n", "<leader>n", "", { desc = "Notes/Obsidian" })
+        vim.keymap.set("n", "<leader>nn", ":ObsidianNew<CR>", { desc = "Obsidian New Note" })
+        vim.keymap.set("n", "<leader>nq", ":ObsidianQuickSwitch<CR>", { desc = "Obsidian Quick Switch" })
+        vim.keymap.set("n", "<leader>nb", ":ObsidianBacklinks<CR>", { desc = "Obsidian Backlinks" })
+        vim.keymap.set("n", "<leader>nft", ":ObsidianTags<CR>", { desc = "Obsidian Find Tags" })
+        vim.keymap.set("n", "<leader>nd", ":ObsidianToday<CR>", { desc = "Obsidian Today" })
+        vim.keymap.set("n", "<leader>nm", ":ObsidianToday +1<CR>", { desc = "Obsidian Tomorrow" })
+        vim.keymap.set("n", "<leader>ny", ":ObsidianToday -1<CR>", { desc = "Obsidian Yesterday" })
+        vim.keymap.set("n", "<leader>nfd", ":ObsidianDailies<CR>", { desc = "Obsidian Find Dailies" })
+        vim.keymap.set("n", "<leader>nt", ":ObsidianTemplate<CR>", { desc = "Obsidian Template" })
+        vim.keymap.set("n", "<leader>nff", ":ObsidianSearch<CR>", { desc = "Obsidian Find" })
+        vim.keymap.set("v", "<leader>nl", ":ObsidianLink<CR>", { desc = "Obsidian Link" })
+        vim.keymap.set("v", "<leader>nw", ":ObsidianLinkNew<CR>", { desc = "Obsidian Link New" })
+        vim.keymap.set("n", "<leader>nfl", ":ObsidianLinks<CR>", { desc = "Obsidian Find Links" })
+        vim.keymap.set("v", "<leader>e", ":ObsidianExtractNote<CR>", { desc = "Obsidian Extract Note" })
+        vim.keymap.set("n", "<leader>nr", ":ObsidianRename<CR>", { desc = "Obsidian Rename" })
+        vim.keymap.set("n", "<leader>nc", ":ObsidianToc<CR>", { desc = "Obsidian ToC" })
+    end,
 }
