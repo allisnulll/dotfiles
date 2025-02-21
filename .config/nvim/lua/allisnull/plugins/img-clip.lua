7bf89b92db9eb397e0cf6b43efa0c54ae7780e7a
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-02-21 03:14:11 -0400

    Add image capability to nvim

---
+++ .config/nvim/lua/allisnull/plugins/img-clip.lua
@@ -0,0 +1,30 @@
+return {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+        default = {
+            dir_path = function()
+                return vim.fn.expand("%:t:r") .. "-img"
+            end,
+            use_absolute_path = false,
+            relative_to_current_file = true,
+            prompt_for_file_name = false,
+            file_name = "%Y-%m-%d-at-%H-%M-%S",
+            extension = "avif",
+            process_cmd = "convert - -quality 75 avif:-",
+        },
+        filetypes = {
+            markdown = {
+                url_encode_path = true,
+                template = "![$FILE_NAME]($FILE_PATH)",
+            },
+        },
+        dirs = {
+            ["~/Vault"] = {
+                dir_path = "assets/images",
+                prompt_for_file_name = true,
+            },
+        },
+    },
+    keys = {{ "<leader>p", ":PasteImage<CR>", desc = "Paste image from system clipboard" }},
+}
