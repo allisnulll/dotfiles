57245e198041608ab080ce695f705ea2c705918a
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-02-23 15:55:32 -0400

    Make timer that commits Obsidian Vault hourly

---
+++ scripts/obsidian_commit.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+REPO_DIR="$HOME/Vault"
+
+if [ ! -d "$REPO_DIR/.git" ]; then
+    echo "Error: Not a git repository: $REPO_DIR"
+    exit 1
+fi
+
+if git diff --quiet; then
+    echo "No changes to commit."
+    exit 0
+else
+    git -C "$REPO_DIR" add .
+    git -C "$REPO_DIR" commit -m "Hourly Commit: $(date '+%Y-%m-%d-%a %H:%M:%S')" && \
+    git -C "$REPO_DIR" push origin main
+fi
