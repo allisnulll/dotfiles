5974001b10ea0e6978ce9adb42c2fc81505d9e1b
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-02-23 06:59:52 -0400

    Make script to switch directional keybinds based on current layout

---
+++ scripts/graphite2qwerty.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+PATCH_FILE="$HOME/.dotfiles/patches/graphite2qwerty.patch"
+REPO_DIR="$HOME/.dotfiles"
+
+if git -C "$REPO_DIR" apply --check -R "$PATCH_FILE" &>/dev/null; then
+    git -C "$REPO_DIR" apply -R "$PATCH_FILE"
+else
+    git -C "$REPO_DIR" apply "$PATCH_FILE"
+fi
+
+tmux source-file "$HOME/.tmux.conf" && tmux display-message "Config reloaded!"
