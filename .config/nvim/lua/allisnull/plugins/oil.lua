db8b6cd4723408097d42057e036128796df4253e
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-02-26 15:07:42 -0400

    Oil.nvim change keymaps

--- .config/nvim/lua/allisnull/plugins/oil.lua
+++ .config/nvim/lua/allisnull/plugins/oil.lua
@@ -34,6 +34,9 @@ return {
                 -- TODO: Figure out how to hide desc
                 ["<C-h>"] = {},
                 ["<C-l>"] = {},
+                ["<C-r>"] = "actions.refresh",
+                ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+                ["<C-v>"] = { "actions.select", opts = { vertical = true } },
                 ["<leader>p"] = function()
                     local oil = require("oil")
                     local filename = oil.get_cursor_entry().name
