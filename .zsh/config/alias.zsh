b8b29a527c017855cea827e4bd2adbbfab2f7e70
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-06-15 15:26:28 -0400

    Add notification test alias

--- .zsh/config/alias.zsh
+++ .zsh/config/alias.zsh
@@ -25,6 +25,7 @@ alias nocolor="sed -r 's/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g'"
 alias copy="wl-copy"
 alias fonts="fc-list --brief | rg 'fullname: \"' | rg -v 'Noto' | sed -r 's/\s+(Bold|Italic|Light|Medium|Thin|ExtraBold|Condensed|Regular|Oblique).*//' | sed 's/fullname: //' | sed 's/\"(s)//' | sed 's/\"//' | sed 's/\t//' | sort | uniq | nocolor"
 alias dae="systemctl --user daemon-reload"
+alias ntest="sudo pkill dunst;c && notify-send 'TITLE' 'low' -u low && notify-send 'TITLE' 'normal' && notify-send 'TITLE' 'critical' -u critical"
 
 # NeoVim
 alias v="nvim"
