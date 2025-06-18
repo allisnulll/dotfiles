98ef3c75006b08d51845439d139569428725fb8b
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-06-18 08:30:02 -0400

    Fix obsidian_commit.sh

--- scripts/obsidian_commit.sh
+++ scripts/obsidian_commit.sh
@@ -7,13 +7,15 @@ if [ ! -d "$REPO_DIR/.git" ]; then
     exit 1
 fi
 
-if git -C "$REPO_DIR" diff --quiet; then
-    echo "No changes to commit."
-    exit 0
-else
+if ! git -C "$REPO_DIR" diff --quiet || \
+   ! git -C "$REPO_DIR" diff --cached --quiet || \
+   [ -n "$(git -C "$REPO_DIR" ls-files --others --exclude-standard)" ]; then
     git -C "$REPO_DIR" add .
     git -C "$REPO_DIR" commit -m "Hourly Commit: $(date '+%Y-%m-%d-%a %H:%M:%S')"
     if [[ "$(nmcli networking connectivity)" == "full" ]]; then
         git -C "$REPO_DIR" push
     fi
+else
+    echo "No changes to commit."
+    exit 0
 fi
