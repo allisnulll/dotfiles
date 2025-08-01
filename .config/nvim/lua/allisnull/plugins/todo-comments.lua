b09c38e9d4f062a8e4b07d3fd9f4155d2f0ade95
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-08-01 19:09:28 -0400

    Re-organize Neovim plugins

--- .config/nvim/lua/allisnull/plugins/todo-comments.lua
+++ .config/nvim/lua/allisnull/plugins/todo-comments.lua
@@ -5,14 +5,17 @@ return {
     config = function()
         local todo_comments = require("todo-comments")
 
-        vim.keymap.set("n", "]t", function()
-            todo_comments.jump_next()
-        end, { desc = "Next todo comment" })
-
-        vim.keymap.set("n", "[t", function()
-            todo_comments.jump_prev()
-        end, { desc = "Previous todo comment" })
+        vim.keymap.set("n", "]t", todo_comments.jump_next, { desc = "Next todo comment" })
+        vim.keymap.set("n", "[t", todo_comments.jump_prev, { desc = "Previous todo comment" })
 
         todo_comments.setup()
-    end
+    end,
 }
+
+-- TODO: test
+-- FIX: test
+-- HACK: test
+-- WARN: test
+-- PERF: test
+-- NOTE: test
+-- TEST: test
--- .config/nvim/lua/allisnull/plugins/ufo.lua
+++ .config/nvim/lua/allisnull/plugins/ufo.lua
@@ -1,16 +1,16 @@
 return {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
+    -- TODO: figure out how to unload nvim-ufo on files with mkview
+    -- event = "User ufo",
     config = function()
         local ufo = require("ufo")
 
-        vim.o.foldcolumn = "0"
+        vim.o.foldcolumn = "1"
         vim.o.foldlevel = 99
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
-
         vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-        vim.o.foldcolumn = "1"
 
         vim.keymap.set("n", "zR", ufo.openAllFolds)
         vim.keymap.set("n", "zM", ufo.closeAllFolds)
