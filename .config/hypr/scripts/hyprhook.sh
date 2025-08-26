becde4ca1cc73261b91c5d4300d9928eda709df0
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-08-26 00:51:14 -0400

    Lazy.nvim update at 2025-08-26-Tue 00:51:14

---
+++ .config/hypr/scripts/hyprhook.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+BWS_FILE=$HOME/.cache/waybar_disabled_ws
+LWS_FILE=$HOME/.cache/last_ws
+[ -f "$BWS_FILE" ] || touch "$BWS_FILE"
+[ -f "$LWS_FILE" ] || echo 1 > "$LWS_FILE"
+
+current_ws=$(hyprctl -j activeworkspace | jq ".id")
+
+cws=$(grep -qw "$current_ws" "$BWS_FILE" && echo 1 || echo 0)
+lws=$(grep -qw $(tail -n 1 "$LWS_FILE") "$BWS_FILE" && echo 1 || echo 0)
+
+case "$1" in
+    bar)
+        if [ "$cws" -eq 1 ]; then
+            sed -i "/^$current_ws$/d" "$BWS_FILE"
+        else
+            echo "$current_ws" >> "$BWS_FILE"
+        fi
+        pkill -x -SIGUSR1 waybar
+        ;;
+
+    start)
+        touch "$BWS_FILE"
+        echo 1 > "$LWS_FILE"
+        ;;
+
+    last)
+        hyprctl dispatch workspace $(head -n 1 "$LWS_FILE")
+        ;;
+
+    *)
+        if [ "$current_ws" -ne $(tail -n 1 "$LWS_FILE") ]; then
+            if [ "$cws" -ne "$lws" ]; then
+                pkill -x -SIGUSR1 waybar
+            fi
+
+            echo "$current_ws" >> "$LWS_FILE"
+            tail -n 2 "$LWS_FILE" > "$LWS_FILE.tmp"
+            mv "$LWS_FILE.tmp" "$LWS_FILE"
+        fi
+        ;;
+esac
