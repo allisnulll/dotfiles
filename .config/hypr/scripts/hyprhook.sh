24f119096a5a98785d9709da80afd7217dd58a40
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-09-01 01:09:47 -0400

    Fix hyprhook.sh

--- .config/hypr/scripts/hyprhook.sh
+++ .config/hypr/scripts/hyprhook.sh
@@ -2,48 +2,47 @@
 
 BWS_FILE=$HOME/.cache/waybar_disabled_ws
 LWS_FILE=$HOME/.cache/last_ws
-[ -f "$BWS_FILE" ] || touch "$BWS_FILE"
-[ -f "$LWS_FILE" ] || echo 1 > "$LWS_FILE"
+[ -f $BWS_FILE ] || touch $BWS_FILE
+[ -s $LWS_FILE ] || echo 1 > $LWS_FILE
 
 current_ws=$(hyprctl -j activeworkspace | jq ".id")
 
-cws=$(grep -qw "$current_ws" "$BWS_FILE" && echo 1 || echo 0)
-lws=$(grep -qw $(tail -n 1 "$LWS_FILE") "$BWS_FILE" && echo 1 || echo 0)
+cws=$(grep -qw $current_ws $BWS_FILE && echo 1 || echo 0)
+lws=$(grep -qw $(tail -n 1 $LWS_FILE) $BWS_FILE && echo 1 || echo 0)
 
-case "$1" in
+case $1 in
     bar)
-        if [ "$cws" -eq 1 ]; then
-            sed -i "/^$current_ws$/d" "$BWS_FILE"
+        if [ $cws -eq 1 ]; then
+            sed -i "/^$current_ws$/d" $BWS_FILE
         else
-            echo "$current_ws" >> "$BWS_FILE"
+            echo $current_ws >> $BWS_FILE
         fi
         pkill -x -SIGUSR1 waybar
         ;;
 
     last)
-        hyprctl dispatch workspace $(head -n 1 "$LWS_FILE")
+        hyprctl dispatch workspace $(head -n 1 $LWS_FILE)
         ;;
 
     start)
-        touch "$BWS_FILE"
-        echo 1 > "$LWS_FILE"
+        touch $BWS_FILE
+        echo 1 > $LWS_FILE
         ;;
 
     off)
         systemctl poweroff
-        rm "$BWS_FILE"
-        rm "$LWS_FILE"
+        rm $BWS_FILE $LWS_FILE
         ;;
 
     *)
-        if [ "$current_ws" -ne $(tail -n 1 "$LWS_FILE") ]; then
-            if [ "$cws" -ne "$lws" ]; then
+        if [ $current_ws -ne $(tail -n 1 $LWS_FILE) ]; then
+            if [ $cws -ne $lws ]; then
                 pkill -x -SIGUSR1 waybar
             fi
 
-            echo "$current_ws" >> "$LWS_FILE"
-            tail -n 2 "$LWS_FILE" > "$LWS_FILE.tmp"
-            mv "$LWS_FILE.tmp" "$LWS_FILE"
+            echo $current_ws >> $LWS_FILE
+            tail -n 2 $LWS_FILE > $LWS_FILE.tmp
+            mv $LWS_FILE.tmp $LWS_FILE
         fi
         ;;
 esac
