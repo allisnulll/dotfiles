b8e067f046b588836ce9dcbf9b712b6ef5f9bb86
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-09-05 18:19:01 -0400

    Set keymap for Nvim Compile Mode

--- .config/nvim/lua/allisnull/plugins/compile-mode.lua
+++ .config/nvim/lua/allisnull/plugins/compile-mode.lua
@@ -4,5 +4,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         vim.g.compile_mode = {}
+
+        vim.keymap.set("n", "<F5>", ":Compile<CR>", { desc = "Compile Mode" })
     end
 }
