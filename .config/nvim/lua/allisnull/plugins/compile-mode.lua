d2efc858ff17cee169623a6b8cc342348da58b4f
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-10-30 02:06:38 -0400

    NeoVim conifigure Compile-Mode

--- .config/nvim/lua/allisnull/plugins/compile-mode.lua
+++ .config/nvim/lua/allisnull/plugins/compile-mode.lua
@@ -3,8 +3,12 @@ return {
     branch = "nightly",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        vim.g.compile_mode = {}
+        vim.g.compile_mode = {
+            ask_about_save = false,
+            ask_to_interrupt = false,
+            auto_jump_to_first_error = true,
+        }
 
-        vim.keymap.set("n", "<F5>", ":Compile<CR>", { desc = "Compile Mode" })
+        vim.keymap.set("n", "<F5>", ":10 Compile<CR>", { desc = "Compile Mode" })
     end
 }
