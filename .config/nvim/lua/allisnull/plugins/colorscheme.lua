472f5880e6416dc3944dfc476d489fa4b5b889e9
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-11-05 16:23:45 -0400

    NeoVim Change IncSearch color

--- .config/nvim/lua/allisnull/plugins/colorscheme.lua
+++ .config/nvim/lua/allisnull/plugins/colorscheme.lua
@@ -12,6 +12,12 @@ return {
             on_colors = function(colors)
                 colors.bg_search = "#660606"
             end,
+            on_highlights = function(highlights, colors)
+                highlights.IncSearch = {
+                    bg = "#990909",
+                    fg = "#000000",
+                }
+            end,
         })
 
         vim.cmd("colorscheme tokyonight")
--- .config/nvimpager/init.lua
+++ .config/nvimpager/init.lua
@@ -322,6 +322,12 @@ require("tokyonight").setup({
     on_colors = function(colors)
         colors.bg_search = "#660606"
     end,
+    on_highlights = function(highlights, colors)
+        highlights.IncSearch = {
+            bg = "#990909",
+            fg = "#000000",
+        }
+    end,
 })
 
 vim.cmd("colorscheme tokyonight")
