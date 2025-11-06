0686f643dd540fc550334d03334eb7f46b049c35
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-11-05 23:30:23 -0400

    NeoVim Mini.Move add arrow keys to mappings

--- .config/nvim/lua/allisnull/plugins/mini/move.lua
+++ .config/nvim/lua/allisnull/plugins/mini/move.lua
@@ -2,5 +2,13 @@ return {
     "echasnovski/mini.move",
     config = function()
         require("mini.move").setup()
+
+        vim.keymap.set("n", "<M-Up>", function() MiniMove.move_line("up") end)
+        vim.keymap.set("n", "<M-Down>", function() MiniMove.move_line("down") end)
+
+        vim.keymap.set("v", "<M-Up>", function() MiniMove.move_selection("up") end)
+        vim.keymap.set("v", "<M-Down>", function() MiniMove.move_selection("down") end)
+        vim.keymap.set("v", "<M-Left>", function() MiniMove.move_selection("left") end)
+        vim.keymap.set("v", "<M-Right>", function() MiniMove.move_selection("right") end)
     end,
 }
