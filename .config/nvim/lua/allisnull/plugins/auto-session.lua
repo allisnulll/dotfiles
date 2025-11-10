8d13b93ca8849ad09dc77a969f20f4c53d19dd02
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-11-09 20:53:17 -0400

    Update AutoSeshion.nvim & Use it instead of :mkview :lo

--- .config/nvim/lua/allisnull/plugins/auto-session.lua
+++ .config/nvim/lua/allisnull/plugins/auto-session.lua
@@ -6,7 +6,7 @@ return {
             auto_session_suppress_dirs = { "~", "~/Dev", "~/Downloads", "~/Documents", "~/Desktop" },
         })
         vim.keymap.set("n", "<leader>w", "", { desc = "Write" })
-        vim.keymap.set("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session for cwd" })
-        vim.keymap.set("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session for auto session root dir" })
+        vim.keymap.set("n", "<leader>ws", ":AutoSession save<CR>", { desc = "Save session for auto session root dir" })
+        vim.keymap.set("n", "<leader>wr", ":AutoSession restore<CR>", { desc = "Restore session for cwd" })
     end,
 }
