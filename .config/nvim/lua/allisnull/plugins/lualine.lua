954f9409f867daa1904caf3163ee1ff7ad43239b
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-12-01 00:02:58 -0400

    NeoVim Update flutter-tools

--- .config/nvim/lua/allisnull/plugins/lualine.lua
+++ .config/nvim/lua/allisnull/plugins/lualine.lua
@@ -1,5 +1,12 @@
 local function maximize_status()
-  return vim.t.maximized and "   " or ""
+    return vim.t.maximized and "   " or ""
+end
+
+local function flutter_app_version()
+    return vim.g.flutter_tools_decorations.app_version
+end
+local function flutter_app_version_exists()
+    return vim.g.flutter_tools_decorations and vim.g.flutter_tools_decorations.app_version ~= nil
 end
 
 return {
@@ -22,9 +29,14 @@ return {
                         cond = lazy_status.has_updates,
                         color = { fg = "#FF9e64" },
                     },
-                    { "encoding" },
-                    { "fileformat" },
-                    { "filetype" },
+                    {
+                        flutter_app_version,
+                        cond = flutter_app_version_exists,
+                        color = { fg = "#5c5c5c" },
+                    },
+                    "encoding",
+                    "fileformat",
+                    "filetype",
                 },
             },
         })
