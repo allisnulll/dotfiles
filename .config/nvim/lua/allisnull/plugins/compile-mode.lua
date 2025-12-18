c5e55a26e74178fa3656ad561cdc94f7492c0665
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-12-18 15:09:05 -0400

    Update compile-mode.nvim

--- .config/nvim/lua/allisnull/plugins/compile-mode.lua
+++ .config/nvim/lua/allisnull/plugins/compile-mode.lua
@@ -4,9 +4,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         vim.g.compile_mode = {
+            default_command = "",
             ask_about_save = false,
             ask_to_interrupt = false,
             auto_jump_to_first_error = true,
+            bang_expansion = true,
         }
 
         vim.keymap.set("n", "<F5>", ":5Compile<CR>", { desc = "Compile Mode" })
