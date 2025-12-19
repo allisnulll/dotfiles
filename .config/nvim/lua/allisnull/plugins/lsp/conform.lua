a860ed265c32aedeaf53e1b95122575927c74e41
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-12-19 05:24:44 -0400

    NeoVim Change Markdown formatter

--- .config/nvim/lua/allisnull/plugins/lsp/conform.lua
+++ .config/nvim/lua/allisnull/plugins/lsp/conform.lua
@@ -16,7 +16,7 @@ return {
                 html = { "prettier" },
                 json = { "prettier" },
                 yaml = { "prettier" },
-                markdown = { "prettier" },
+                markdown = { "mdformat" },
                 php = { "pint" },
             },
         })
