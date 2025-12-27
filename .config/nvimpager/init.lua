c44b862b33758a955d7861db13e1a592b438ac23
allisnulll <133504785+allisnulll@users.noreply.github.com>
2025-12-26 20:41:12 -0400

    Fix NvimPager Config

--- .config/nvimpager/init.lua
+++ .config/nvimpager/init.lua
@@ -243,7 +243,7 @@ vim.pack.add({
     { src = "https://github.com/folke/which-key.nvim" },
     { src = "https://github.com/folke/zen-mode.nvim" },
     { src = "https://github.com/folke/todo-comments.nvim" },
-    { src = "https://github.com/norcalli/nvim-colorizer.lua" }, -- NOTE: Checkout 260bc9d2f56ad5333df53938835c69639ef0f452
+    { src = "https://github.com/norcalli/nvim-colorizer.lua" }, -- NOTE: run `git fetch pull/115/head:pr-115 && git checkout 260bc9d2f56ad5333df53938835c69639ef0f452`
     { src = "https://github.com/chentoast/marks.nvim" },
     { src = "https://github.com/echasnovski/mini.ai" },
     { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
@@ -251,7 +251,7 @@ vim.pack.add({
 })
 
 -- Treesitter
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter.config").setup({
     highlight = { enable = true },
     auto_install = true,
     ensure_installed = {
