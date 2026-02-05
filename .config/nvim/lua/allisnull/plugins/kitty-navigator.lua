4f40c8c73a88f998c29d9bb188b3170ce283bb0c
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-02-05 05:44:11 -0400

    Update vim-kitty-navigator.nvim

--- .config/nvim/lua/allisnull/plugins/kitty-navigator.lua
+++ .config/nvim/lua/allisnull/plugins/kitty-navigator.lua
@@ -2,8 +2,5 @@ return {
     "knubie/vim-kitty-navigator",
     event = "VeryLazy",
     build = "cp ./*.py ~/.config/kitty/",
-    cond = function()
-        local term = os.getenv("TERM")
-        return term and string.find(term, "kitty")
-    end
+    cond = os.getenv("TERM") and string.find(os.getenv("TERM"), "kitty")
 }
