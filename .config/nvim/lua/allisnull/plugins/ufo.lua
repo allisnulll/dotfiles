3de7d3b3d1a872d4b8329c4b1b7e362a1ec982bb
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-02-05 05:40:20 -0400

    NeoVim properly setup fold options & ZenMode plugin

--- .config/nvim/lua/allisnull/plugins/ufo.lua
+++ .config/nvim/lua/allisnull/plugins/ufo.lua
@@ -6,12 +6,6 @@ return {
     config = function()
         local ufo = require("ufo")
 
-        vim.o.foldcolumn = "1"
-        vim.o.foldlevel = 99
-        vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
-        vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-
         vim.keymap.set("n", "zR", ufo.openAllFolds)
         vim.keymap.set("n", "zM", ufo.closeAllFolds)
         vim.keymap.set("n", "<C-f>", function()
--- .config/nvim/lua/allisnull/plugins/zen-mode.lua
+++ .config/nvim/lua/allisnull/plugins/zen-mode.lua
@@ -8,40 +8,30 @@ return {
                     signcolumn = "no",
                     colorcolumn = "0",
                     wrap = true,
+                    foldcolumn = "1",
+                    foldlevel = 99,
+                    foldlevelstart = 99,
+                    foldenable = true,
+                    fillchars = "foldopen:,foldclose:,eob: ,fold: ,foldsep: ,foldinner: ",
                 },
             },
-            plugins = {
-                enable = true,
-                rules = false,
-                showcmd = false,
-            },
-            gitsigns = { enabled = false },
 
             on_open = function(_)
                 -- TODO: Figure out alternative to unloading which-key to disable it on zen-mode https://github.com/folke/which-key.nvim/discussions/510
-                vim.opt.hlsearch = false
                 vim.fn.system("tmux set status off")
                 vim.fn.system('tmux list-panes -F "\\#F" | grep -q Z || tmux resize-pane -Z')
                 vim.fn.system("kitten @ set-font-size +2")
                 vim.diagnostic.enable(false)
-
-                vim.keymap.set("n", "<M-j>", "gj", { noremap = true, silent = true, buffer = true })
-                vim.keymap.set("n", "<M-k>", "gk", { noremap = true, silent = true, buffer = true })
             end,
+
             on_close = function(_)
-                vim.opt.hlsearch = true
                 vim.fn.system("tmux set status on")
                 vim.fn.system('tmux list-panes -F "\\#F" | grep -q Z && tmux resize-pane -Z')
                 vim.fn.system("kitten @ set-font-size 0")
                 vim.diagnostic.enable()
-
-                if not vim.fn.getcwd():match("/home/allisnull/Vault") then
-                    vim.api.nvim_buf_del_keymap(0, "n", "<M-j>")
-                    vim.api.nvim_buf_del_keymap(0, "n", "<M-k>")
-                end
             end,
         })
 
-        vim.keymap.set("n", "<leader>zm", ":ZenMode<CR>", { desc = "Enter ZenMode" })
+        vim.keymap.set("n", "<M-z>", ":ZenMode<CR>", { desc = "Enter ZenMode" })
     end,
 }
