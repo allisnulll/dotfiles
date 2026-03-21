e2a65590805db287ccc0032beba487ede01683e5
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-03-21 00:53:05 -0400

    Add Valgrind

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ stow --no-folding .
 cd ~ || exit
 
 sudo ln -sf ~/.dotfiles/pacman.conf /etc
-paru -Sy --needed fastfetch eza zoxide btop htop yazi powertop tree tmux go sesh-bin fzf ripgrep fd jq wget tealdeer cmake clang gdb nodejs npm lua51 rustup pyenv python-pynvim python-pip php composer tree-sitter-cli xdg-user-dirs noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra kanata-git opencode
+paru -Sy --needed fastfetch eza zoxide btop htop yazi powertop tree tmux go sesh-bin fzf ripgrep fd jq wget tealdeer cmake clang gdb valgrind nodejs npm lua51 rustup pyenv python-pynvim python-pip php composer tree-sitter-cli xdg-user-dirs noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra kanata-git opencode
 
 xdg-user-dirs-update
 
