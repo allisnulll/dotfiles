c83f66dc9489052d628b585ef1db0561acc4a418
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-03-26 05:12:31 -0400

    NeoVim Update auto-session.nvim

--- .config/nvim/lua/allisnull/plugins/auto-session.lua
+++ .config/nvim/lua/allisnull/plugins/auto-session.lua
@@ -3,10 +3,10 @@ return {
     config = function()
         require("auto-session").setup({
             auto_restore_enabled = false,
-            auto_session_suppress_dirs = { "~", "~/Dev", "~/Downloads", "~/Documents", "~/Desktop" },
+            auto_session_suppress_dirs = { "~", "~/Downloads", "~/Documents", "~/Desktop" },
         })
         vim.keymap.set("n", "<leader>w", "", { desc = "Write" })
-        vim.keymap.set("n", "<leader>ws", ":AutoSession save<CR>", { desc = "Save session for auto session root dir" })
+        vim.keymap.set("n", "<leader>ww", ":AutoSession save<CR>", { desc = "Save session for auto session root dir" })
         vim.keymap.set("n", "<leader>wr", ":AutoSession restore<CR>", { desc = "Restore session for cwd" })
     end,
 }
