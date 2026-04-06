ecb5157b5510e30cf38c1c8a1e386b8e5d7a8270
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-04-06 11:27:39 -0400

    GtkLock Add multiple monitor support

--- .config/gtklock/lock.sh
+++ .config/gtklock/lock.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-grim /tmp/lock.png
-corrupter /tmp/lock.png /tmp/lock.png
-gtklock -dc ~/.config/gtklock/config.ini -s ~/.config/gtklock/style.css --idle-hide --idle-timeout 30
+for monitor in $(hyprctl monitors -j | jq -r '.[].name'); do
+    grim -o "$monitor" "/tmp/lock-${monitor}.png"
+    corrupter "/tmp/lock-${monitor}.png" "/tmp/lock-${monitor}.png"
+done
+
+cat ~/.config/gtklock/style.css > /tmp/gtklock-monitors.css
+
+for monitor in $(hyprctl monitors -j | jq -r '.[].name'); do
+    echo "
+window#${monitor} {
+    background-image: url(\"/tmp/lock-${monitor}.png\");
+}" >> /tmp/gtklock-monitors.css
+done
+
+gtklock -d -c ~/.config/gtklock/config.ini -s /tmp/gtklock-monitors.css --follow-focus --idle-hide --idle-timeout 30
