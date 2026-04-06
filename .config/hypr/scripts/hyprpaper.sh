8fb76961a5c4c869bf85375253949f94b07f8f9c
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-04-06 08:08:53 -0400

    Hyprland Update HyprPaper script

--- .config/hypr/scripts/hyprpaper.sh
+++ .config/hypr/scripts/hyprpaper.sh
@@ -2,22 +2,24 @@
 
 WALLPAPER_DIR="$HOME/Pictures/wallpapers"
 WALLPAPER="$WALLPAPER_DIR/alucard.png"
+CACHE_DIR="/tmp/hyprpaper"
 
 if [[ $1 == start ]]; then
-    hyprctl hyprpaper preload "$WALLPAPER"
+    mkdir -p "$CACHE_DIR"
     for monitor in $(hyprctl monitors -j | jq -r ".[] | .name"); do
         hyprctl hyprpaper wallpaper "$monitor,$WALLPAPER"
+        echo "$WALLPAPER" > "$CACHE_DIR/$monitor"
     done
 
     exit
 fi
 
 ACTIVE_MONITOR=$(hyprctl monitors -j | jq -r ".[] | select(.focused == true) | .name")
+CACHE_FILE="$CACHE_DIR/$ACTIVE_MONITOR"
 
 if [[ $1 != default ]]; then
-    CURRENT_WALLPAPER=$(basename "$(hyprctl hyprpaper listloaded)")
-    WALLPAPER=$(fd --type f . "$WALLPAPER_DIR" | rg -v "alucard.png|$CURRENT_WALLPAPER" | shuf -n 1)
+    WALLPAPER=$(fd --type f . "$WALLPAPER_DIR" | rg -v "alucard.png|$(cat "$CACHE_FILE")" | shuf -n 1)
 fi
 
-hyprctl hyprpaper preload "$WALLPAPER"
 hyprctl hyprpaper wallpaper "$ACTIVE_MONITOR,$WALLPAPER"
+echo "$WALLPAPER" > "$CACHE_FILE"
