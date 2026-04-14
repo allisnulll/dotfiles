abe6acef3eb1adaefbc037695d791b9f9c33a129
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-04-14 08:36:15 -0400

    rofi-web-search.sh use OmniSearch

--- scripts/rofi-web-search.sh
+++ scripts/rofi-web-search.sh
@@ -2,7 +2,7 @@
 # NOTE: Taken from https://github.com/gimslab/rofi-web-search
 
 if [[ ${ROFI_RETV:-0} -eq 2 ]]; then
-    xdg-open "https://www.duckduckgo.com/search?q=$@" > /dev/null 2>&1
+    xdg-open "http://localhost:8087/search?q=$@" > /dev/null 2>&1
     exit 0
 fi
 echo Search web... 
