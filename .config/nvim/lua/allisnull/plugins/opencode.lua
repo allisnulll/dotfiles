a63d2a646007bfb36f152d43f818275dd28c59fa
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-04-16 03:20:19 -0400

    Update OpenCode.nvim keymaps

--- .config/nvim/lua/allisnull/plugins/opencode.lua
+++ .config/nvim/lua/allisnull/plugins/opencode.lua
@@ -43,6 +43,7 @@ return {
 
         vim.keymap.set({ "n", "x" }, "<M-a><M-a>", function() opencode.select() end, { desc = "Execute opencode action…" })
         vim.keymap.set({ "n", "x" }, "<M-a>a", function() opencode.ask("@this: ", { submit = true }) end, { desc = "Ask opencode" })
+        vim.keymap.set({ "n", "x" }, "<M-a>A", function() opencode.prompt("@this") end, { desc = "Add to opencode" })
         vim.keymap.set({ "n", "x" }, "<M-a>b", function() opencode.ask("@buffer: ", { submit = true }) end, { desc = "Ask opencode about buffer" })
         vim.keymap.set({ "n", "x" }, "<M-a>B", function() opencode.ask("@buffers: ", { submit = true }) end, { desc = "Ask opencode about all buffers" })
         vim.keymap.set({ "n", "x" }, "<M-a>v", function() opencode.ask("@visible: ", { submit = true }) end, { desc = "Ask opencode about visible text" })
@@ -50,8 +51,7 @@ return {
         vim.keymap.set({ "n", "x" }, "<M-a>q", function() opencode.ask("@quickfix: ", { submit = true }) end, { desc = "Ask opencode about quickfix list" })
         vim.keymap.set({ "n", "x" }, "<M-a>d", function() opencode.ask("@diff: ", { submit = true }) end, { desc = "Ask opencode about diff" })
 
-        vim.keymap.set({ "n", "x" }, "gA", function() opencode.prompt("@this") end, { desc = "Add to opencode" })
-        vim.keymap.set({ "n", "t" }, "<M-.>", opencode.toggle, { desc = "Toggle opencode" })
+        vim.keymap.set({ "n", "x" }, "<M-a>t", opencode.toggle, { desc = "Toggle opencode" })
 
         vim.keymap.set({ "n", "i" }, "<M-PageUp>", function() opencode.command("session.page.up") end, { desc = "opencode page up" })
         vim.keymap.set({ "n", "i" }, "<M-PageDown>", function() opencode.command("session.page.down") end, { desc = "opencode page down" })
