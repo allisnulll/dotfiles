b46517649d84ea3a78cad08069d1d1a14d3ef19e
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-05-02 07:02:37 -0400

    NeoVim Update tree-sitter configuration

--- .config/nvim/lua/allisnull/plugins/treesitter.lua
+++ .config/nvim/lua/allisnull/plugins/treesitter.lua
@@ -1,47 +1,36 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            highlight = { enable = true },
-            indent = {
-                enable = true,
-                disable = { "scheme" },
-            },
-            auto_install = true,
-            ensure_installed = {
-                "json",
-                "javascript",
-                "yaml",
-                "toml",
-                "html",
-                "css",
-                "gitignore",
-                "java",
-                "dart",
-                "bash",
-                "c",
-                "lua",
-                "markdown",
-                "latex",
-                "python",
-                "vim",
-                "vimdoc",
-                "query",
-                "scheme",
-                "commonlisp",
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-Space>",
-                    node_incremental = "<C-Space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
+        require("nvim-treesitter").install({
+            "json",
+            "javascript",
+            "yaml",
+            "toml",
+            "html",
+            "css",
+            "gitignore",
+            "java",
+            "dart",
+            "bash",
+            "c",
+            "lua",
+            "markdown",
+            "latex",
+            "python",
+            "vim",
+            "vimdoc",
+            "query",
+            "scheme",
+            "commonlisp",
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "scheme",
+            callback = function()
+                vim.bo.indentexpr = ""
+            end,
         })
     end
 }
