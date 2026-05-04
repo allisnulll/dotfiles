be82caf6fef508a77e3ebc18e9a28ab8cb0c1b74
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-05-04 18:33:45 -0400

    Vault git pull --rebase changes before commiting

--- scripts/obsidian_commit.sh
+++ scripts/obsidian_commit.sh
@@ -10,6 +10,16 @@ fi
 if ! git -C "$REPO_DIR" diff --quiet || \
    ! git -C "$REPO_DIR" diff --cached --quiet || \
     [[ -n $(git -C "$REPO_DIR" ls-files --others --exclude-standard) ]]; then
+    if [[ "$(nmcli networking connectivity)" == "full" ]]; then
+        BRANCH=$(git -C "$REPO_DIR" rev-parse --abbrev-ref HEAD)
+        git -C "$REPO_DIR" fetch
+        if git -C "$REPO_DIR" log HEAD.."origin/$BRANCH" --oneline | grep -q .; then
+            git -C "$REPO_DIR" stash
+            git -C "$REPO_DIR" pull --rebase
+            git -C "$REPO_DIR" stash apply
+            git -C "$REPO_DIR" stash drop
+        fi
+    fi
     git -C "$REPO_DIR" add .
     git -C "$REPO_DIR" commit -m "Hourly Commit: $(date '+%Y-%m-%d-%a %H:%M:%S')"
     if [[ "$(nmcli networking connectivity)" == "full" ]]; then
