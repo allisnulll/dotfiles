eaaa5fa6a4f8fb71ab307a3c48d5e5e8a661c7c2
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-05-04 16:38:05 -0400

    Yazi Give sudo permissions

--- .zsh/config/function.zsh
+++ .zsh/config/function.zsh
@@ -126,7 +126,7 @@ chromium-view() {
 # Yazi
 function y() {
 	local tmp=$(mktemp -t yazi-cwd.XXXXXX) cwd
-	yazi $@ --cwd-file=$tmp
+	sudo yazi $@ --cwd-file=$tmp
 	IFS= read -r -d "" cwd < $tmp
 	[ -n $cwd ] && [ $cwd != $PWD ] && builtin cd -- $cwd
 	rm -f -- $tmp
