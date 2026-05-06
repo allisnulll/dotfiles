9a73c7f3e22477d48f6bfb414f9997a12b76352a
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-05-06 08:23:10 -0400

    WayBar Fix time module

--- .config/waybar/scripts/clock-time.sh
+++ .config/waybar/scripts/clock-time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HOUR_12=$(( $(date +%H) % 12 ))
+HOUR_12=$(( 10#$(date +%H) % 12 ))
 ICONS=(󱑖 󱑋 󱑌 󱑍 󱑎 󱑏 󱑐 󱑑 󱑒 󱑓 󱑔 󱑕)
 TIME=$(date +%H:%M)
 echo "{\"text\": \" ${ICONS[$HOUR_12]} $TIME\"}"
