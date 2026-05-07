9fafe243701514d3c67393f24e9dbc1028667ca5
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-05-07 06:11:38 -0400

    Yazi Fix

--- .zsh/config/function.zsh
+++ .zsh/config/function.zsh
@@ -126,9 +126,9 @@ chromium-view() {
 # Yazi
 function y() {
 	local tmp=$(mktemp -t yazi-cwd.XXXXXX) cwd
-	sudo yazi $@ --cwd-file=$tmp
-	IFS= read -r -d "" cwd < $tmp
-	[ -n $cwd ] && [ $cwd != $PWD ] && builtin cd -- $cwd
+	sudo -E yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d "" cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
 	rm -f -- $tmp
 }
 
